095eb1cb4ce48ff0c8e4444fe56de619e18dfe46
Davouciii <dav4thevid@gmail.com>
2024-03-26 21:04:50 +0100

    used middle to check validation of id

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const tourController = require("./../controllers/tourController");
 
-
-
 const router = express.Router();
 
-router.route("/").get(tourController.getAllTours).post(tourController.postATour);
-router.route("/:id").get(tourController.getATour).patch(tourController.updateATour).delete(tourController.deleteATour);
+router.param('id', tourController.checkID)
+
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.postATour);
+router
+  .route("/:id")
+  .get(tourController.getATour)
+  .patch(tourController.updateATour)
+  .delete(tourController.deleteATour);
 
 module.exports = router;
