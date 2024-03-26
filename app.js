bda3f098bcec410d781177348d4e225b574f7b33
Davouciii <dav4thevid@gmail.com>
2024-03-26 23:45:45 +0100

    enhan: read the public static page

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
-
 const app = express();
 
 const tourRouter = require("./routes/tourRoutes");
 const usersRouter = require("./routes/userRoutes");
 
-
 //MIDDLEWARES
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
@@ -21,7 +20,6 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", usersRouter);
