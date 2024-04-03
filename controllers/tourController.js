69596313354c29f940c11a1a0dbdaed1a6a3a211
Davouciii <dav4thevid@gmail.com>
2024-04-03 23:48:47 +0100

    added sorting fields, fields limiting and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,42 @@
 const Tour = require("./../models/tourModels");
 
 exports.getAllTours = async (req, res) => {
-  //BUILD QUERY
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "sort", "limit", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
   try {
-    const query = Tour.find(req.query);
+    //BUILD QUERY
+
+    //1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1b) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort(-"createdAt");
+    }
+
+    // 3) Fields Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
 
     // const query = Tour.find()
     //   .where("duration")
